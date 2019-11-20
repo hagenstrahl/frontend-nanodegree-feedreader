8cb0a06b8d95e73432de6ab9782d40e7aed3f7d8
Hagen Strahl <hagen.strahl@googlemail.com>
2019-11-20 16:06:36 +0100

    fixed bug: secondFeed was undefined

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -118,7 +118,6 @@ $(function () {
                     secondFeed = document.querySelector('.feed').innerHTML;
                     done();
                 });
-                done();
             })
 
         });
